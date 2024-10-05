eed6fd7d4a26b315199fa8131f8518c6ffe58089
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-10-04 19:31:24 -0700

    update seeding function, README, and add a link to another page when done

--- README.md
+++ README.md
@@ -15,23 +15,34 @@
 
 ### Backend packages used:
 Results from pip freeze:
-annotated-types==0.7.0
-anyio==4.6.0
-bcrypt==4.2.0
-fastapi==0.115.0
-greenlet==3.1.1
-idna==3.10
-passlib==1.7.4
-pydantic==2.9.2
-pydantic_core==2.23.4
-PyJWT==2.9.0
-python-dotenv==1.0.1
-python-multipart==0.0.12
-sniffio==1.3.1
-SQLAlchemy==2.0.35
-starlette==0.38.6
-typing_extensions==4.12.2
+- annotated-types==0.7.0
+- anyio==4.6.0
+- bcrypt==4.2.0
+- fastapi==0.115.0
+- greenlet==3.1.1
+- idna==3.10
+- passlib==1.7.4
+- pydantic==2.9.2
+- pydantic_core==2.23.4
+- PyJWT==2.9.0
+- python-dotenv==1.0.1
+- python-multipart==0.0.12
+- sniffio==1.3.1
+- SQLAlchemy==2.0.35
+- starlette==0.38.6
+- typing_extensions==4.12.2
 
 if uvicorn backend.main:app --reload doesnt work, make sure you have all of these packages installed.
 
+## Frontend
+Just run "npm install" inside of the frontend directory to install all the packages to use the nextjs app locally
+
 ### Currently the CORS are only set for local developmnent. Need to be updated if site is deployed
+
+### Note
+When deciding whether or not I was going to store the items clicked on the frontend or backend I chose to go with the frontend to keep the project scoped to around 5 hours of work. This was not a oversight, it was a deliberate omission.
+
+- Pros
+    - Quick implementation
+- Cons:
+    - When page refreshes user loses data of images clicked
--- frontend/app/hunt/page.tsx
+++ frontend/app/hunt/page.tsx
@@ -1,8 +1,8 @@
-//Need to make page only accessible if user is signed in
 'use client'
 import { useEffect, useState } from "react"
 import { get_Items } from "../utils/itemsAPI"
 import Image from "next/image"
+import Link from "next/link"
 
 interface Item {
     title: string
@@ -13,7 +13,7 @@ interface Item {
 const page: React.FC = () => {
     const [items, setItems] = useState<Item[]>([])
     const [error, setError] = useState<string | null>(null)
-
+    const [selectedItems, setSelectedItems] = useState<number[]>([])
 
     useEffect(() => {
         const fetchItems = async () => {
@@ -21,44 +21,77 @@ const page: React.FC = () => {
                 const data = await get_Items()
                 setItems(data)
             }
-            catch(error) {
-                if(error instanceof Error) {
+            catch (error) {
+                if (error instanceof Error) {
                     setError(error.message)
                 }
             }
-
         }
         fetchItems()
     }, [])
 
+    const handleClick = (id: number) => {
+        if (selectedItems.includes(id)) {
+            // Remove the clicked item if already in array
+            setSelectedItems(selectedItems.filter(itemId => itemId !== id));
+        } else {
+            // Add item to array
+            setSelectedItems([...selectedItems, id]);
+        }
+    }
 
-  return (
-    <div className="flex flex-col justify-center items-center">
-        <div className="text-2xl">Scavenger Hunt Page</div>
+    return (
+        <div className="flex flex-col justify-center items-center mb-20">
+            <div className="my-4 text-center">
+                <h1 className="text-3xl font-bold">Scavenger Hunt</h1>
+                <h5 className="text-xl font-semibold mt-4">Found {selectedItems.length} out of {items.length} items</h5>
+                <h6 className="">Explore Davis and find all these plants and animals. When you find one click on the picture to check it off.</h6>
+                <h6 className="font-semibold">Find them all for a special suprise... 👀</h6>
+            </div>
 
-        {error && <h3>Error: {error}</h3>}
+            {error && <h3>Error: {error}</h3>}
 
-        <div className="">
-            {items.map((item) => {
-                return (
-                    
-                    <div key={item.id}>
-                        <h1 className="text-lg">Title: {item.title}</h1>
+            <div className={`${items.length ? "grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-10 lg:grid-cols-3 lg:gap-20" : "grid grid-cols-1"}`}>
+                {items.map((item) => {
+                    const isClicked = selectedItems.includes(item.id);
+                    return (
+                        <div key={item.id} className="flex flex-col justify-center items-center">
+                            <h1 className="text-lg font-semibold text-center">Title: {item.title}</h1>
 
-                        <Image
-                            src={item.photo_url}
-                            width={500}
-                            height={500}
-                            alt="Picture of a plant or animal around davis california"
-                            />
+                            <div className="relative rounded-xl overflow-hidden" onClick={() => handleClick(item.id)}>
+                                <Image
+                                    src={item.photo_url}
+                                    width={250}
+                                    height={250}
+                                    alt="Picture of a plant or animal around davis california"
+                                    className={`rounded-xl bg-black p-2 ${isClicked ? 'opacity-60' : 'opacity-100'}`}
+                                    priority
+                                />
+                                {isClicked && (
+                                    <div className="absolute inset-0 bg-black opacity-50" />
+                                )}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
 
+            {selectedItems.length === items.length && (
+                    <div className="mt-4 sm:mt-10 flex flex-col justify-center items-center w-full h-full">
+                        <h1 className="text-xl font-bold text-center">You have completed the scavanger hunt, click continue.</h1>
+                        
+                        <Link href={'/congratulations'}>
+                        <button className="bg-black text-white p-3 mt-2 rounded font-bold text-lg hover:bg-opacity-80">
+                            Continue
+                        </button>
+                        </Link>
                     </div>
-                )
-            })}
-        </div>
 
-    </div>
-  )
+                )}
+
+
+        </div>
+    )
 }
 
 export default page
